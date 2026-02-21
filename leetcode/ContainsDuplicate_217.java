871b76858238f8ec2e654bd2c1ec919556295f97
David Castaneda <david9castaneda@gmail.com>
2026-02-21 11:50:49 -0600

    217 - Contains Duplicate

---
+++ leetcode/ContainsDuplicate_217.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// Time: O(n)
+// Space: O(1)
+// Intuition: Keep track of seen elements using a set
+public class ContainsDuplicate_217 {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+
+        System.out.printf("Contains duplicate? %s", Solution(nums));
+    }
+
+    static boolean Solution(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+
+        return false;
+    }
+}
