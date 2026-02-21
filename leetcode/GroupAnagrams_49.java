cec6c5a8adb675bb83f4c2506147d20cbb048aa4
David Castaneda <david9castaneda@gmail.com>
2026-02-21 17:36:01 -0600

    49 - Group Anagrams

--- leetcode/GroupAnagrams_49.java
+++ leetcode/GroupAnagrams_49.java
@@ -8,8 +8,8 @@ import java.util.Map;
 
 // https://leetcode.com/problems/group-anagrams/
 //
-// Time: O(n)
-// Space: O(n)
+// Time: O(n * mlogm)
+// Space: O(m * n)
 // Intuition: Sort each word, use the sorted word as the key in a map.
 // The value will be a list of every string that maps to the sorted key.
 public class GroupAnagrams_49 {
@@ -18,9 +18,11 @@ public class GroupAnagrams_49 {
         String[] input = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
 
         Map<String, List<String>> map = new HashMap<>();
+
+        // O(n)
         for (String s : input) {
             char[] c = s.toCharArray();
-            Arrays.sort(c);
+            Arrays.sort(c); // O(mlogm)
             String key = new String(c);
             map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
         }
